015231b506d6d1eeb8d67a19c35e3e7a350a755d
Seongho Lee <tjdgh2626@gmail.com>
2021-06-10 11:25:39 +0000

    Add react-router-dom package and apply basic routing

--- linetracer-comp-frontend/src/App.js
+++ linetracer-comp-frontend/src/App.js
@@ -1,14 +1,50 @@
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-import EntryForm from './EntryForm'
+import LinetracerCompEntryForm from './forms/entry/LinetracerCompEntryForm';
 
 function App() {
 	return (
-		<Container>
-			<h1>Linetracer Competition Page</h1>
-			<EntryForm />
-		</Container>
+		<Router>
+			<Container>
+				<Navbar bg="light" expand="lg">
+					<Navbar.Brand href="/">ZETIN Competition</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Link className="nav-link" to="/">Home</Link>
+							<Link className="nav-link" to="/entry">Entry</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				
+				<Switch>
+					<Route path="/entry">
+						<LinetracerCompEntryForm />
+					</Route>
+					
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+				
+			</Container>
+    </Router>
+	);
+}
+
+function Home() {
+	return (
+		<h1>Welcome to ZETIN Competition Page</h1>
 	);
 }
 
