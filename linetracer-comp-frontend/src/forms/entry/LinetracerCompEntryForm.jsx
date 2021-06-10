e49cb010066622788a381f03081291a2f18f76db
Seongho Lee <tjdgh2626@gmail.com>
2021-06-10 11:24:11 +0000

    Change name of EntryForm.js to forms/entry/LinetracerCompEntryForm.jsx and update it

--- linetracer-comp-frontend/src/forms/entry/LinetracerCompEntryForm.jsx
+++ linetracer-comp-frontend/src/forms/entry/LinetracerCompEntryForm.jsx
@@ -2,43 +2,48 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-class EntryForm extends React.Component {
+class LinetracerCompEntryForm extends React.Component {
 	constructor(props) {
 		super(props);
-		
 		this.number = props.number;
+		this.formColumn = 2;
 	}
 
 	render() {
 		return (
 			<Form>
-				<h2>인적 사항</h2>
+				<h3>인적 사항</h3>
+				<p className="text-muted">참가자의 정보를 입력해주세요.</p>
 				<Form.Group controlId="formName">
 					<Form.Label>이름</Form.Label>
-					<Form.Control type="name" placeholder="" />
+					<Form.Control type="name" />
 				</Form.Group>
 				<Form.Group controlId="formEmail">
 					<Form.Label>이메일</Form.Label>
-					<Form.Control type="email" placeholder="" />
-					<Form.Text className="text-muted">
-						이메일은 대회의 안내 사항을 전하는데 사용됩니다.
-					</Form.Text>
+					<Form.Control type="email" />
+					<Form.Text className="text-muted">이메일은 대회의 안내 사항을 전하는데 사용됩니다.</Form.Text>
 				</Form.Group>
-				<h2>로봇 정보</h2>
+				<Form.Group controlId="formTeam">
+					<Form.Label>소속</Form.Label>
+					<Form.Control type="text" />
+					<Form.Text className="text-muted">예시: 서울시립대학교 ZETIN</Form.Text>
+				</Form.Group>
+				<h3>로봇 정보</h3>
+				<p className="text-muted">로봇에 대한 정보를 입력해주세요.</p>
 				<Form.Group controlId="formCPU">
 					<Form.Label>CPU</Form.Label>
-					<Form.Control type="text" placeholder="CPU 모델명을 입력해주세요." />
-				</Form.Group>
-				<Form.Group controlId="formROM">
-					<Form.Label>롬(ROM)</Form.Label>
-					<Form.Control type="text" placeholder="" />
+					<Form.Control type="text" />
 				</Form.Group>
 				<Form.Group controlId="formRAM">
-					<Form.Label>램(RAM)</Form.Label>
+					<Form.Label>RAM</Form.Label>
+					<Form.Control type="text" placeholder="" />
+				</Form.Group>
+				<Form.Group controlId="formROM">
+					<Form.Label>ROM</Form.Label>
 					<Form.Control type="text" placeholder="" />
 				</Form.Group>
 				<Form.Group controlId="formMotorDriver">
-					<Form.Label>모터 드라이버</Form.Label>
+					<Form.Label>Motor Driver</Form.Label>
 					<Form.Control type="text" placeholder="" />
 				</Form.Group>
 				<Form.Group controlId="formADC">
@@ -46,7 +51,7 @@ class EntryForm extends React.Component {
 					<Form.Control type="text" placeholder="" />
 				</Form.Group>
 				<Form.Group controlId="formSensor">
-					<Form.Label>센서</Form.Label>
+					<Form.Label>Sensor</Form.Label>
 					<Form.Control type="text" placeholder="" />
 				</Form.Group>
 				<Button variant="primary" type="submit">제출</Button>
@@ -55,4 +60,4 @@ class EntryForm extends React.Component {
 	}
 }
 
-export default EntryForm;
+export default LinetracerCompEntryForm;
