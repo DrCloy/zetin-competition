f8489e83477a2d7386e50fa47d9479d3782449a5
Seongho Lee <tjdgh2626@gmail.com>
2021-07-24 13:59:41 +0000

    Update ComeptitionCardView.jsx

--- client/src/components/CompetitionCardView.jsx
+++ client/src/components/CompetitionCardView.jsx
@@ -7,20 +7,14 @@ import Col from 'react-bootstrap/Col';
 
 class CompetitionCardView extends React.Component {
   render() {
-    const cols = this.props.data.map((value) => (
-      <Col className="p-2" key={value._id}>
-        <CompetitionCard
-          name={value.name}
-          desc={value.desc}
-          poster={`/files/posters/${value._id}`}
-        />
-      </Col>
-    ));
-
     return (
       <>
         <Row xs={1} sm={2} lg={4}>
-          {cols}
+          {this.props.data.map((value) => (
+            <Col className="p-2" key={value._id}>
+              <CompetitionCard data={value} />
+            </Col>
+          ))}
         </Row>
       </>
     );
