99a9726675d5d9749df1f101924aa5d72205952c
Seongho Lee <tjdgh2626@gmail.com>
2021-07-25 09:25:45 +0000

    Attach corresponding competition page link and fetch a poster

--- client/src/components/CompetitionCard.jsx
+++ client/src/components/CompetitionCard.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Card from 'react-bootstrap/Card';
 
+import './CompetitionCard.css';
+
 class CompetitionCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      src: `/files/posters/${props.data._id}`,
+      errored: false,
+    };
+
+    this.handleError = this.handleError.bind(this);
+  }
+
+  handleError() {
+    if (!this.state.errored) {
+      this.setState({
+        src: 'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+        errored: true,
+      });
+    }
+  }
+
   render() {
-    const { name, desc, poster } = this.props;
+    const { _id, name } = this.props.data;
+    const { src } = this.state;
 
     return (
-      <Card>
-        <Card.Img variant="top" src={poster} />
-        <Card.Body>
+      <Card className="card-competition" as={Link} to={`/competitions/${_id}`}>
+        <Card.Img variant="top" src={src} onError={this.handleError} />
+        <Card.Body className="text-dark">
           <Card.Title>{name}</Card.Title>
-          <Card.Text>{desc}</Card.Text>
         </Card.Body>
       </Card>
     );
