f2ab330dbdc4e79b58b8395be8b4720f450ffe65
Seongho Lee <tjdgh2626@gmail.com>
2021-07-25 11:09:09 +0000

    Implement API for getting thumbnail images

--- routes/files/poster.js
+++ routes/files/poster.js
@@ -1,6 +1,7 @@
 /* Dependencies */
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 const mime = require('mime-types');
 const router = require('express').Router();
 const multer = require('multer');
@@ -8,6 +9,9 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 /* Constants */
 const PATH = path.join(process.env.PATH_FILES, 'posters');
+const THUMB_PATH = path.join(PATH, 'thumbnails');
+const THUMB_FORMAT = 'jpg';
+const THUMB_WIDTH = 720;
 
 // get poster iamge path by id regardless of file extension
 function getPosterPath(id) {
@@ -75,9 +79,44 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+// get thumbnail image
+router.get('/thumbnail/:id', (req, res) => {
+  const thumbnail = path.join(THUMB_PATH, `${req.params.id}.${THUMB_FORMAT}`);
+
+  fs.access(thumbnail, fs.constants.R_OK, (err) => {
+    if (err) {
+      // create and response thumbnail image
+      getPosterPath(req.params.id)
+        .then((path) => {
+          sharp(path)
+            .resize({ width: THUMB_WIDTH })
+            .toFormat(THUMB_FORMAT)
+            .toFile(thumbnail)
+            .then(() => {
+              res.sendFile(thumbnail);
+            })
+            .catch((err) => {
+              res.status(500).send(err.toString());
+            });
+        })
+        .catch((err) => {
+          res.status(500).send(err.toString());
+        });
+    } else {
+      // response thumbnail image
+      res.sendFile(thumbnail);
+    }
+  });
+});
+
 module.exports = (function () {
   // make directory if it doesn't exist.
   fs.mkdir(PATH, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  // make thumbnail directory
+  fs.mkdir(THUMB_PATH, { recursive: true }, (err) => {
     if (err) throw err;
   });
 
