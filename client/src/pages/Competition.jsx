9603dee02c6739663f6669f0ce1fc5cd9ab782a5
Seongho Lee <tjdgh2626@gmail.com>
2021-08-03 14:23:26 +0000

    Change class component to functional component(with Hooks)

--- client/src/pages/Competition.jsx
+++ client/src/pages/Competition.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 import Button from 'react-bootstrap/Button';
@@ -8,104 +9,81 @@ import CompetitionView from '../components/CompetitionView';
 import CompetitionForm from '../forms/CompetitionForm/CompetitionForm';
 import PageNotFound from '../components/PageNotFound';
 
-class Competition extends React.Component {
-  constructor(props) {
-    super(props);
+function Competition() {
+  const [competition, setCompetition] = useState(null);
+  const [isError, setIsError] = useState(false);
+  const [showEditingModal, setShowEditingModal] = useState(false);
 
-    this.state = {
-      data: null,
-      isDataLoaded: false,
-      showEditingModal: false,
-    };
-  }
+  const { id } = useParams();
+  const history = useHistory();
 
-  componentDidMount() {
-    const id = this.props.match.params.id;
+  // get the competition document correspond to the id
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`/api/competitions/${id}`);
+        setCompetition(data);
+      } catch (err) {
+        setCompetition(null);
+        setIsError(true);
+      }
+    })();
+  }, [id]);
 
-    if (!this.state.isDataLoaded) {
-      let data = null;
-      axios
-        // get competition information for id
-        .get(`/api/competitions/${id}`)
-        .then((value) => {
-          data = { ...value.data };
-        })
-        .catch(console.error)
-        .finally(() => this.setState({ data, isDataLoaded: true }));
-    }
-  }
-
-  handleDeleteCompetition = () => {
-    const { history } = this.props;
-    const { _id } = this.state.data;
-
-    axios
-      // delete competition db
-      .delete(`/api/competitions/${_id}`)
-      // delete competition poster
-      .then(() => axios.delete(`/files/posters/${_id}`))
-      .then(() => {
-        alert('대회 페이지를 삭제했습니다.');
+  // delete the competition document
+  const handleDeleteCompetition = () => {
+    (async () => {
+      try {
+        const { data } = await axios.delete(`/api/competitions/${id}`);
+        if (data.posterId) {
+          await axios.delete(`/api/files/${data.posterId}`);
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
         history.push('/');
-      })
-      .catch(console.error);
+      }
+    })();
   };
 
-  handleCloseModal = () => {
-    this.setState({ showEditingModal: false });
-  };
-
-  render() {
-    const { data, isDataLoaded, showEditingModal } = this.state;
-
-    return (
-      <>
-        {isDataLoaded ? (
-          data ? (
-            <>
-              <CompetitionView data={data} />
-              <div className="fixed-bottom w-100 bg-light border-top">
-                <div className="container p-3">
-                  <div className="text-right">
-                    <Button
-                      variant="primary"
-                      onClick={() => {
-                        this.setState({ showEditingModal: true });
-                      }}
-                    >
-                      수정
-                    </Button>{' '}
-                    <Button
-                      variant="danger"
-                      onClick={this.handleDeleteCompetition}
-                    >
-                      삭제
-                    </Button>
-                  </div>
-                </div>
-              </div>
-              <Modal
-                size="lg"
-                show={showEditingModal}
-                onHide={this.handleCloseModal}
-                backdrop="static"
-                keyboard={false}
+  return (
+    <>
+      {competition !== null ? (
+        <>
+          <CompetitionView data={competition} />
+          <div className="fixed-bottom w-100 bg-light border-top">
+            <div className="container p-3 text-right">
+              <Button
+                variant="primary"
+                onClick={() => setShowEditingModal(true)}
               >
-                <Modal.Header closeButton>
-                  <Modal.Title>경연 대회 페이지 수정</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <CompetitionForm competition={data} />
-                </Modal.Body>
-              </Modal>
-            </>
-          ) : (
-            <PageNotFound />
-          )
-        ) : null}
-      </>
-    );
-  }
+                수정
+              </Button>{' '}
+              <Button variant="danger" onClick={handleDeleteCompetition}>
+                삭제
+              </Button>
+            </div>
+          </div>
+          <Modal
+            size="lg"
+            show={showEditingModal}
+            onHide={() => setShowEditingModal(false)}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>경연 대회 페이지 수정</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <CompetitionForm competition={competition} />
+            </Modal.Body>
+          </Modal>
+        </>
+      ) : isError ? (
+        <PageNotFound />
+      ) : null}
+    </>
+  );
 }
 
 export default Competition;
