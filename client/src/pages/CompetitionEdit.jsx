0524c7ebc635e32ce42459e9541816917299744c
Seongho Lee <tjdgh2626@gmail.com>
2021-08-06 14:12:38 +0000

    Add CompetitionEdit Page for creation or modification of competition

---
+++ client/src/pages/CompetitionEdit.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import PageWrapper from './PageWrapper';
+import CompetitionForm from '../forms/CompetitionForm/CompetitionForm';
+import PageNotFound from '../components/PageNotFound';
+
+function CompetitionEdit() {
+  const { id } = useParams();
+
+  const [competition, setCompetition] = useState(null);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (id) {
+      // modification mode
+      axios
+        // get the competition document correspond to the id
+        .get(`/api/competitions/${id}`)
+        .then((res) => {
+          setCompetition(res.data);
+          setIsError(false);
+        })
+        .catch((err) => {
+          setCompetition(null);
+          setIsError(err);
+        });
+    }
+  }, [id]);
+
+  if (isError) return <PageNotFound />;
+  if (!id)
+    return <PageWrapper title="대회 페이지 개설" page={<CompetitionForm />} />;
+  if (competition)
+    return (
+      <PageWrapper
+        title="대회 페이지 수정"
+        page={<CompetitionForm competition={competition} />}
+      />
+    );
+
+  return null;
+}
+
+export default CompetitionEdit;
