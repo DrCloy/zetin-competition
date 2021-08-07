cd45538e0e415306d210aee7f61f51a1d79875aa
Seongho Lee <tjdgh2626@gmail.com>
2021-08-07 13:23:33 +0000

    Add multiLine attribute

--- client/src/forms/fields/TextField.jsx
+++ client/src/forms/fields/TextField.jsx
@@ -3,15 +3,21 @@ import Form from 'react-bootstrap/Form';
 import { useField } from 'formik';
 
 const TextField = (props) => {
-  const { label, controlId } = props;
+  const { label, controlId, multiLine, rows } = props;
 
   const [field, meta] = useField(props.name);
   const { touched, error } = meta;
 
+  field.isInvalid = touched && error;
+
   return (
     <Form.Group controlId={controlId}>
       <Form.Label>{label}</Form.Label>
-      <Form.Control type="text" isInvalid={touched && error} {...field} />
+      {multiLine ? (
+        <Form.Control as="textarea" rows={rows || 4} {...field} />
+      ) : (
+        <Form.Control type="text" {...field} />
+      )}
       <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
       {props.children}
     </Form.Group>
