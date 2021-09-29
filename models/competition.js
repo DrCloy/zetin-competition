cb322a1da2d24290bb7ef016f9b1dc281b5413e3
Seongho Lee <tjdgh2626@gmail.com>
2021-09-29 14:04:03 +0000

    Add a participate method of Competition document

--- models/competition.js
+++ models/competition.js
@@ -29,5 +29,38 @@ const competitionSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+competitionSchema.methods.participate = async function (participant) {
+  const { _id, eventId, entryOrder } = participant;
+
+  // find event
+  let event = null;
+  for (let i = 0; i < this.events.length; i++) {
+    if (this.events[i]._id.toString() === eventId) {
+      event = this.events[i];
+      break;
+    }
+  }
+
+  // Error: Not found
+  if (!event) {
+    return `해당 경연 부문이 존재하지 않습니다.`;
+  }
+  // Error: Over the limit
+  if (entryOrder >= event.numb) {
+    return `정원을 ${event.numb}명을 초과했습니다.`;
+  }
+  // Error: Already exist
+  if (event._participants[entryOrder]) {
+    return `해당 순번은 이미 사용중입니다.`;
+  }
+
+  // save
+  event._participants[entryOrder] = _id;
+  this.markModified('events');
+  await this.save();
+
+  return 0;
+};
+
 // Create Model & Export
 module.exports = mongoose.model('Competition', competitionSchema);
