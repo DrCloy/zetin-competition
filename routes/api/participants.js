6fb7273f28acb54508026d53c093b0204ee76def
Seongho Lee <tjdgh2626@gmail.com>
2021-10-06 14:07:43 +0000

    Update participants GET API

--- routes/api/participants.js
+++ routes/api/participants.js
@@ -8,8 +8,42 @@ const Participant = require('../../models/participant');
 
 router.get('/', async (req, res, next) => {
   try {
-    let collection = await Participant.find({});
+    const { competitionId, dateSort } = req.query;
+
+    const filter = {};
+    if (competitionId) {
+      filter._competitionId = competitionId;
+    }
+
+    const projection = {
+      _competitionId: 1,
+      _eventId: 1,
+      name: 1,
+      team: 1,
+      robotName: 1,
+      createdAt: 1,
+    };
+
+    const options = {
+      sort: { createdAt: dateSort || -1 },
+    };
+
+    let collection = await Participant.find(filter, projection, options);
     res.json(collection);
+  } catch (err) {
+    next(createError(500, err));
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    let participant = await Participant.findById(req.params.id);
+
+    if (participant) {
+      res.json(participant);
+    } else {
+      return next(createError(404, 'Participant not found'));
+    }
   } catch (err) {
     next(createError(500, err));
   }
