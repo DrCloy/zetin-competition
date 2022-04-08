f0252743fcb10fdd8eda64b6b41af8a7011f1c1c
Seongho Lee <tjdgh2626@gmail.com>
2022-04-08 22:18:53 +0900

    Add administrator check logic using admin middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,28 +2,43 @@ const router = require('express').Router();
 const axios = require('axios');
 const createError = require('http-errors');
 
-router.post('/login/zetin', async (req, res, next) => {
-  try {
-    const { id, pw } = req.body;
+const admin = require('../../middlewares/admin')();
 
-    if (!id || !pw) {
-      next(createError(401, 'Invalid id or pw.'));
+router.post(
+  '/admin',
+  // first, zetin member authentication
+  async (req, res, next) => {
+    try {
+      const { id, pw } = req.body;
+      const resLogin = await axios.post(
+        // login
+        'https://auth.zetin.uos.ac.kr/auth',
+        {
+          id,
+          pw,
+        },
+      );
+
+      const { status, token } = resLogin.data;
+      if (status === 'success') {
+        // for input of admin middleware
+        req.headers['authorization'] = token;
+        next();
+      } else {
+        next(
+          createError(401, 'ZETIN Login was failed. Please check ID and PW.'),
+        );
+      }
+    } catch (err) {
+      next(createError(500, err));
     }
-
-    const resZetinAuth = await axios.post('https://auth.zetin.uos.ac.kr/auth', {
-      id,
-      pw,
-    });
-
-    const { status, token } = resZetinAuth.data;
-    if (status === 'success') {
-      res.status(200).send(token);
-    } else {
-      next(createError(401, 'Login at the zetin server was failed.'));
-    }
-  } catch (err) {
-    next(createError(500, err));
-  }
-});
+  },
+  // second, check this member is admin. if not call 401 http error.
+  admin,
+  // third, if this member is admin, send authentication information.
+  (req, res) => {
+    res.status(200).send(req.headers['authorization']);
+  },
+);
 
 module.exports = router;
