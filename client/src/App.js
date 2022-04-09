80ab07c03b5ee9a69b04735d684301def1cb6823
Seongho Lee <tjdgh2626@gmail.com>
2022-04-09 16:08:29 +0900

    Remove CompetitionEdit page (Intergrate with Admin page)

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Admin from './pages/Admin';
 import Competition from './pages/Competition';
-import CompetitionEdit from './pages/CompetitionEdit';
 import Rule from './pages/Rule';
 import Entry from './pages/Entry';
 import Participant from './pages/Participant';
@@ -13,10 +12,6 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route
-          path={['/competitions/edit/:id', '/competitions/edit']}
-          component={CompetitionEdit}
-        />
         <Route path="/competitions/:id" component={Competition} />
         <Route path={['/entry/:competitionId']} component={Entry} />
         <Route path="/participants/:competitionId" component={Participant} />
