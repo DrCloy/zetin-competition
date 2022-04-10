4d90a4954a3583c67ae3fb05b878ca64d7571458
Seongho Lee <tjdgh2626@gmail.com>
2022-04-10 22:04:20 +0900

    Add Password mongoose model for hashing and storing password

---
+++ models/password.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const passwordSchema = new mongoose.Schema({
+  targetId: { type: String, required: true },
+  digest: { type: String, required: true },
+  hash: { type: String, required: true },
+  iteration: { type: Number },
+  salt: { type: Number },
+});
+
+passwordSchema.statics.verify = async function (targetId, plain) {
+  const password = await this.findOne({ targetId });
+
+  if (!password) return null;
+  if (!plain) return null;
+
+  const match = await bcrypt.compare(plain, password.hash);
+  if (match) return password;
+
+  return null;
+};
+
+passwordSchema.statics.findByTargetId = async function (targetId) {
+  let password = await this.findOne({ targetId });
+  if (!password) return null;
+  return password;
+};
+
+module.exports = mongoose.model('Password', passwordSchema);
