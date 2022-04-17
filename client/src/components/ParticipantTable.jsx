74ccef5c92f65ab185a451bc4b6ba6f356819dd4
Seongho Lee <tjdgh2626@gmail.com>
2022-04-17 15:10:17 +0900

    Add pagination of participant table

--- client/src/components/ParticipantTable.jsx
+++ client/src/components/ParticipantTable.jsx
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
 
 const ParticipantTable = (props) => {
   const { competition, onParticipantClick } = props;
   const [participants, setParticipants] = useState([]);
+  const [activePage, setActivePage] = useState(0);
+
+  const countPerPage = props.countPerPage || 5;
 
   useEffect(() => {
     async function getParticipants() {
       const res = await axios.get('/api/participants', {
         params: {
           competitionId: competition._id,
-          dateSort: 'asc',
+          dateSort: 'desc',
         },
       });
 
@@ -24,38 +28,61 @@ const ParticipantTable = (props) => {
     }
   }, [competition]);
 
-  return (
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>성명</th>
-          <th>소속</th>
-          <th>로봇명</th>
-        </tr>
-      </thead>
-      <tbody>
-        {participants.map((value, index) => {
-          const { _id, name, team, robotName } = value;
-          return (
-            <tr
-              key={_id}
-              onClick={
-                onParticipantClick &&
-                (() => {
-                  onParticipantClick(value);
-                })
-              }
+  const paginationItems = [];
+  for (let i = 0; i < participants.length / countPerPage; i++) {
+    paginationItems.push(
+      <Pagination.Item
+        key={i}
+        active={i === activePage}
+        onClick={() => setActivePage(i)}
+      >
+        {i + 1}
+      </Pagination.Item>,
+    );
+  }
+
+  const offset = activePage * countPerPage;
+  const tableRows = participants
+    .slice(offset, offset + countPerPage)
+    .map((value, index) => {
+      const { _id, name, team, robotName } = value;
+      return (
+        <tr key={_id}>
+          <td>{participants.length - index - offset}</td>
+          <td>{name}</td>
+          <td>{team}</td>
+          <td>
+            <a
+              href={`#${robotName}`}
+              onClick={(e) => {
+                e.preventDefault();
+                onParticipantClick && onParticipantClick(value);
+              }}
             >
-              <td>{index + 1}</td>
-              <td>{name}</td>
-              <td>{team}</td>
-              <td>{robotName}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+              {robotName}
+            </a>
+          </td>
+        </tr>
+      );
+    });
+
+  return (
+    <div>
+      <Table striped bordered hover size="sm" className="text-center">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>성명</th>
+            <th>소속</th>
+            <th>로봇명</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </Table>
+      <Pagination size="sm" className="justify-content-center">
+        {paginationItems}
+      </Pagination>
+    </div>
   );
 };
 
