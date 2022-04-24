9ca888e5d61bc7b9b2f61ea2992a3d546121bb74
Seongho Lee <tjdgh2626@gmail.com>
2022-04-25 01:48:46 +0900

    Add participant manipulation functions

--- client/src/routes/Participants.jsx
+++ client/src/routes/Participants.jsx
@@ -2,14 +2,19 @@ import { useState, useEffect } from 'react';
 import { useOutletContext, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 
+import Button from 'react-bootstrap/Button';
+
 import ParticipantTable from '../components/ParticipantTable';
 import ParticipantView from '../components/ParticipantView';
+import ParticipantAuthForm from '../forms/ParticipantAuthForm';
+import EntryForm from '../forms/EntryForm';
 
 export default function Participants() {
   const { competition } = useOutletContext();
-  const [searchParams, setSearchParams] = useSearchParams();
   const [participant, setParticipant] = useState(null);
-
+  const [modification, setModification] = useState(null);
+  const [entryFormData, setEntryFormData] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
   const participantId = searchParams.get('pid');
 
   useEffect(() => {
@@ -24,7 +29,85 @@ export default function Participants() {
   }, [participantId]);
 
   return participant ? (
-    <ParticipantView participant={participant} />
+    modification ? (
+      entryFormData ? (
+        <EntryForm {...entryFormData} />
+      ) : (
+        <>
+          <p>{modification.message}</p>
+          <ParticipantAuthForm
+            participant={participant}
+            method={modification.method}
+            onSucceed={modification.onSucceed}
+            onCancelled={() => setModification(null)}
+          />
+        </>
+      )
+    ) : (
+      <>
+        <div className="text-right">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => setSearchParams({})}
+          >
+            돌아가기
+          </Button>
+        </div>
+        <ParticipantView participant={participant} />
+        <div className="text-right">
+          <Button
+            variant="primary"
+            size="sm"
+            onClick={() => {
+              setModification({
+                method: 'PATCH',
+                message: '참가 신청 내용을 수정하려면 비밀번호를 입력해주세요.',
+                onSucceed: async (data) => {
+                  const { password } = data;
+                  const resParticipant = await axios.get(
+                    // get more information with authentication
+                    `/api/participants/${participantId}`,
+                    { headers: { Authorization: password } },
+                  );
+                  setEntryFormData({
+                    competition,
+                    data: resParticipant.data,
+                    password,
+                    onSubmitted: (res) => {
+                      setParticipant(res.data);
+                      setModification(null);
+                      setEntryFormData(null);
+                    },
+                  });
+                },
+              });
+            }}
+          >
+            수정
+          </Button>{' '}
+          <Button
+            variant="danger"
+            size="sm"
+            onClick={() =>
+              setModification({
+                method: 'DELETE',
+                message: '참가 신청을 취소하려면 비밀번호를 입력해주세요.',
+                onSucceed: async (data) => {
+                  const { password } = data;
+                  await axios.delete(`/api/participants/${participantId}`, {
+                    headers: { Authorization: password },
+                  });
+                  setModification(null);
+                },
+              })
+            }
+          >
+            참가 취소
+          </Button>
+        </div>
+      </>
+    )
   ) : (
     <ParticipantTable
       competition={competition}
@@ -33,6 +116,7 @@ export default function Participants() {
       }}
       countPerPage={10}
       searchParamName={'pid'}
+      className={participant && 'd-none'}
     />
   );
 }
