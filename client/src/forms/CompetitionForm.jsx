3ee657dbd042a31847e5aad28b9f75656bff3af1
Seongho Lee <tjdgh2626@gmail.com>
2022-06-09 00:09:38 +0900

    Update CompetitionForm using react-hook-form

--- client/src/forms/CompetitionForm.jsx
+++ client/src/forms/CompetitionForm.jsx
@@ -1,25 +1,23 @@
 /* Dependencies */
-import React, { useState, useEffect } from 'react';
-import { Formik, Form as FormikForm, useFormikContext } from 'formik';
-import axios from 'axios';
+import { useEffect } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import moment from 'moment';
+import axios from 'axios';
 
 /* Bootstrap Components */
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-/* Field & Input Components */
-import DateField from './fields/DateField';
-import EventField from './fields/EventField';
-import ImageInput from '../components/ImageInput';
-import MarkdownField from './fields/MarkdownField';
-import SelectField from './fields/SelectField';
-import TextField from './fields/TextField';
+/* Input Components */
+import Input from './components/Input';
+import EventInput from './components/EventInput';
+import DateInput from './components/DateInput';
+import MarkdownTextArea from './components/MarkdownTextArea';
+import FieldStack from './components/FieldStack';
 
-// initial form values
-const initialValues = {
+const defaultValues = {
   name: '',
   desc: '',
   events: [],
@@ -36,8 +34,7 @@ const initialValues = {
   posterId: '',
 };
 
-// validation schema with yup
-const yupSchema = yup.object({
+const schema = yup.object({
   name: yup.string().required('대회 이름을 입력해주세요.'),
   events: yup
     .array()
@@ -46,7 +43,6 @@ const yupSchema = yup.object({
   regDateStart: yup.string().required('참가 신청 접수 시작일을 입력해주세요.'),
   regDateEnd: yup
     .string()
-    .required('참가 신청 접수 종료일을 입력해주세요.')
     .test(
       'minDateTime',
       '참가 신청 접수 시작일보다 빠를 수 없습니다.',
@@ -65,256 +61,109 @@ const yupSchema = yup.object({
   googleMap: yup.string().url('URL 형식이 올바르지 않습니다.'),
 });
 
-// CompetitionForm component
-const CompetitionForm = (props) => {
-  const { data, token, onSubmitSuccess } = props;
+export default function CompetitionForm(props) {
+  const { data, onSubmitted } = props;
 
-  const [poster, setPoster] = useState(null);
-  const [posterChanged, setPosterChanged] = useState(false);
-  const [rules, setRules] = useState([]);
+  const form = useForm({ defaultValues, resolver: yupResolver(schema) });
+  const { handleSubmit, reset } = form;
 
-  useEffect(() => {
-    const getPosterBlob = (posterId) => {
-      return new Promise((resolve, reject) => {
-        axios
-          .get(`/api/files/${posterId}`, { responseType: 'blob' })
-          .then((res) => resolve(res.data))
-          .catch((err) => reject(err));
-      });
-    };
-
-    // get rules & poster image
-    (async () => {
-      let ruleResponse = [];
-      let posterBlob = null;
-
-      try {
-        ruleResponse = await axios.get(`/api/rules`);
-        if (data && !!data.posterId) {
-          posterBlob = await getPosterBlob(data.posterId);
-        }
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setPoster(posterBlob);
-        setPosterChanged(false);
-        setRules(ruleResponse.data);
-      }
-    })();
-  }, [data]);
-
-  const handleSubmit = async (competition, formikBag) => {
-    let posterDoc = null;
-    let competitionDoc = null;
-
+  const onSubmit = async (values) => {
     try {
-      // upload poster image file
-      if (posterChanged) {
-        if (poster) {
-          const fileForm = new FormData();
+      let response;
 
-          fileForm.set('file', poster);
-          if (!competition.posterId) {
-            // create new poster document
-            fileForm.set('category', 'poster');
-            fileForm.set('description', competition.name);
-            posterDoc = await axios.post(`/api/files`, fileForm);
-            // set posterId to competition document
-            competition.posterId = posterDoc.data._id;
-          } else {
-            // modify poster document (change image file)
-            posterDoc = await axios.patch(
-              `/api/files/${competition.posterId}`,
-              fileForm,
-            );
-          }
-        } else {
-          if (competition.posterId) {
-            // delete poster document
-            posterDoc = await axios.delete(
-              `/api/files/${competition.posterId}`,
-            );
-            competition.posterId = '';
-          }
-        }
-      }
-
-      // post competition values
       if (!data) {
-        // create new competition document
-        competitionDoc = await axios.post(`/api/competitions`, competition, {
-          headers: { authorization: `${token}` },
-        });
+        response = await axios.post('/api/competitions', values);
       } else {
-        // modify competition document
-        competitionDoc = await axios.patch(
-          `/api/competitions/${data._id}`,
-          competition,
-          {
-            headers: { authorization: `${token}` },
-          },
-        );
+        response = await axios.patch(`/api/competitions/${data._id}`, values);
       }
 
-      onSubmitSuccess && onSubmitSuccess(competitionDoc.data._id);
-      formikBag.setStatus(undefined);
+      onSubmitted && onSubmitted(response);
     } catch (err) {
-      formikBag.setStatus(err);
+      window.alert(err.response?.message);
     }
   };
 
+  useEffect(() => {
+    if (data) reset(data);
+  }, [reset, data]);
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={yupSchema}
-      onSubmit={handleSubmit}
-    >
-      <FormikForm noValidate>
-        <FormikEffect data={data} />
-        <TextField label="이름" name="name" controlId="compName" />
-        <MarkdownField label="설명" name="desc" controlId="compDesc" />
-        <EventField label="경연 부문" name="events" controlId="compEvents" />
-        <Row>
-          <Col md>
-            <DateField
-              label="참가 신청 접수 시작일"
-              name="regDateStart"
-              controlId="compRegDateStart"
-            />
-          </Col>
-          <Col md>
-            <DateField
-              label="참가 신청 접수 종료일"
-              name="regDateEnd"
-              controlId="compRegDateEnd"
-            />
-          </Col>
-          <Col md>
-            <DateField
-              label="대회 개최일"
-              name="date"
-              controlId="compDate"
-              enableTime
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col md>
-            <TextField label="장소" name="place" controlId="compPlace" />
-          </Col>
-          <Col md>
-            <TextField
-              label="구글 지도"
-              name="googleMap"
-              controlId="compGoogleMap"
-              advice={
-                <>
-                  <a
-                    href="https://www.google.com/maps/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    구글 지도
-                  </a>
-                  의 "공유 &gt; 지도 퍼가기"에서 iframe의 src 속성을
-                  입력해주세요.
-                </>
-              }
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col md>
-            <TextField
-              label="주최 및 주관"
-              name="organizer"
-              controlId="compOrganizer"
-            />
-          </Col>
-          <Col md>
-            <TextField label="후원" name="sponser" controlId="compSponser" />
-          </Col>
-        </Row>
-        <MarkdownField
+    <FormProvider {...form}>
+      <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Input type="text" label="이름" name="name" id="cmpName" />
+        <MarkdownTextArea label="설명" name="desc" id="cmpDesc" rows={12} />
+        <EventInput label="경연 부문" name="events" id="cmpEvents" />
+        <FieldStack>
+          <DateInput
+            label="참가 신청 접수 시작"
+            name="regDateStart"
+            id="cmpRegDateStart"
+          />
+          <DateInput
+            label="참가 신청 접수 종료"
+            name="regDateEnd"
+            id="cmpRegDateEnd"
+          />
+          <DateInput label="대회 개최일" name="date" id="cmpDate" />
+        </FieldStack>
+        <FieldStack>
+          <Input type="text" label="장소" name="place" id="cmpPlace" />
+          <Input
+            type="text"
+            label="구글 지도"
+            name="googleMap"
+            id="cmpGoogleMap"
+            advice={
+              <>
+                <a
+                  href="https://www.google.com/maps/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  구글 지도
+                </a>
+                의 "공유 &gt; 지도 퍼가기"에서 iframe의 src 속성을 입력해주세요.
+              </>
+            }
+          />
+        </FieldStack>
+        <FieldStack>
+          <Input
+            type="text"
+            label="주최 및 주관"
+            name="organizer"
+            id="cmpOrganizer"
+          />
+          <Input type="text" label="후원" name="sponser" id="cmpSponser" />
+        </FieldStack>
+        <MarkdownTextArea
           label="시상 내역"
           name="prize"
-          controlId="compPrize"
-          rows="4"
+          id="cmpPrize"
+          rows={4}
         />
-        <SelectField
+        <Input
+          type="text"
           label="대회 규정"
           name="rule"
-          options={
-            rules &&
-            rules.map((value) => [
-              value._id,
-              `${value.name} (${value.version})`,
-            ])
-          }
-          controlId="compRule"
+          id="cmpRule"
+          advice="대회 규정 파일 ID를 입력해주세요."
         />
-        <MarkdownField
+        <MarkdownTextArea
           label="추가 정보"
           name="moreInfo"
-          controlId="compMoreInfo"
+          id="cmpMoreInfo"
+          rows={8}
         />
-        <ImageInput
+        <Input
+          type="text"
           label="대회 포스터"
-          value={poster}
-          onChange={(e) => {
-            setPoster(e);
-            setPosterChanged(true);
-          }}
-          controlId="compPoster"
+          name="posterId"
+          id="cmpPosterId"
+          advice="대회 포스터 파일 ID를 입력해주세요."
         />
-        <hr />
-        <SubmitButton />
-      </FormikForm>
-    </Formik>
+        <Button type="submit">제출</Button>
+      </Form>
+    </FormProvider>
   );
-};
-
-// side effect processing component
-const FormikEffect = (props) => {
-  const { data } = props;
-  const { resetForm } = useFormikContext();
-
-  // side effect for changing competition data
-  useEffect(() => {
-    resetForm({ values: { ...initialValues, ...data } });
-  }, [data, resetForm]);
-
-  return null;
-};
-
-// submit button component
-const SubmitButton = (props) => {
-  const { submitForm, isSubmitting, isValid, submitCount, status } =
-    useFormikContext();
-
-  let error = '';
-  if (!isValid && !!submitCount) {
-    error = '대회 페이지 입력이 유효하지 않습니다.';
-  } else if (status instanceof Error) {
-    error = status.response
-      ? status.response.data
-      : '알 수 없는 오류가 발생했습니다.';
-  }
-
-  return (
-    <>
-      <Button
-        className="mr-3"
-        disabled={isSubmitting}
-        onClick={!isSubmitting ? submitForm : null}
-      >
-        {isSubmitting ? '제출 중...' : '제출'}
-      </Button>
-      {error ? (
-        <p className="text-danger d-inline align-middle">{error}</p>
-      ) : null}
-    </>
-  );
-};
-
-export default CompetitionForm;
+}
