822593e5d6476105151c68f68e2835f1f534d242
Seongho Lee <tjdgh2626@gmail.com>
2022-06-10 02:56:49 +0900

    Add MarkdownIt instance which add target=_blank attribute to the anchor tag

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import MarkdownIt from 'markdown-it';
 
 // https://www.codegrepper.com/code-examples/javascript/javascript+bytes+to+human+readable+size
 function formatBytes(bytes, decimals = 2) {
@@ -26,4 +27,33 @@ function checkDateTerm(comp, start, end) {
   else return false;
 }
 
-export { formatBytes, formatDate, checkDateTerm };
+// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+const markdown = new MarkdownIt();
+// Remember old renderer, if overridden, or proxy to default renderer
+let defaultRender =
+  markdown.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+markdown.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // If you are sure other plugins can't add `target` - drop check below
+  var aIndex = tokens[idx].attrIndex('target');
+  var rIndex = tokens[idx].attrIndex('rel');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank'; // replace value of existing attr
+  }
+
+  if (rIndex < 0) {
+    tokens[idx].attrPush(['rel', 'noreferrer']);
+  } else {
+    tokens[idx].attrs[aIndex][1] = 'noreferrer';
+  }
+
+  // pass token to default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
+
+export { formatBytes, formatDate, checkDateTerm, markdown };
