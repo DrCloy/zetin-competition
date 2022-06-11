b71f91267cf8b19dfa879ac2646748cf216dd77c
Seongho Lee <tjdgh2626@gmail.com>
2022-06-11 11:20:34 +0900

    Seperate manipulation component to user render function

--- client/src/components/ParticipantTable.jsx
+++ client/src/components/ParticipantTable.jsx
@@ -1,52 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Pagination from 'react-bootstrap/Pagination';
 
-/*
- < Properties >
- * competition: a competition document
- * onParticipantClick: participant click handler
- * searchParamName: set name of query parameter, default is 'pid' 
- */
-const ParticipantTable = (props) => {
+export default function ParticipantTable(props) {
   const {
-    competition,
-    onParticipantClick,
-    searchParamName,
+    data,
+    renderFunction,
+    renderHref,
+    onClick,
     countPerPage,
     ...restProps
   } = props;
-  const [participants, setParticipants] = useState(null);
+
   const [activePage, setActivePage] = useState(0);
-
-  useEffect(() => {
-    async function getParticipants() {
-      const res = await axios.get('/api/participants', {
-        params: {
-          competitionId: competition._id,
-          dateSort: 'desc',
-        },
-      });
-
-      setParticipants(res.data);
-    }
-
-    if (competition) {
-      getParticipants();
-    }
-  }, [competition]);
-
   let tableRows;
-  let paginationItems;
+  const paginationItems = [];
   const count = props.countPerPage || 5;
   const offset = activePage * count;
 
-  if (participants) {
+  if (data) {
     // for pagination
-    paginationItems = [];
-    for (let i = 0; i < participants.length / countPerPage; i++) {
+    for (let i = 0; i < data.length / countPerPage; i++) {
       paginationItems.push(
         <Pagination.Item
           key={i}
@@ -59,61 +33,47 @@ const ParticipantTable = (props) => {
     }
 
     // for table rows
-    tableRows = participants
-      .slice(offset, offset + count)
-      .map((value, index) => {
-        const { _id, name, team, robotName } = value;
-        return (
-          <tr key={_id}>
-            <td>{participants.length - index - offset}</td>
-            <td>{name}</td>
-            <td>{team}</td>
-            <td>
-              <a
-                href={'?' + (searchParamName || 'pid') + '=' + _id}
-                onClick={(e) => {
-                  e.preventDefault();
-                  onParticipantClick && onParticipantClick(_id);
-                }}
-              >
-                {robotName}
-              </a>
-            </td>
-          </tr>
-        );
-      });
-  } else {
-    tableRows = [];
-    for (let i = 0; i < count; i++) {
-      tableRows.push(
-        <tr key={i}>
-          <td>-</td>
-          <td>-</td>
-          <td>-</td>
-          <td>-</td>
-        </tr>,
+    tableRows = data.slice(offset, offset + count).map((value, index) => {
+      const { _id, name, team, robotName } = value;
+      return (
+        <tr key={_id}>
+          <td>{data.length - index - offset}</td>
+          <td>{name}</td>
+          <td>{team}</td>
+          <td>
+            <a
+              href={renderHref ? renderHref(value) : '#'}
+              onClick={(e) => {
+                e.preventDefault();
+                onClick && onClick(value);
+              }}
+            >
+              {robotName}
+            </a>
+          </td>
+          {renderFunction ? <td>{renderFunction(value)}</td> : null}
+        </tr>
       );
-    }
+    });
   }
 
   return (
     <div {...restProps}>
-      <Table striped bordered hover size="sm" className="text-center">
+      <Table striped hover size="sm" className="text-center">
         <thead>
           <tr>
             <th>#</th>
             <th>성명</th>
             <th>소속</th>
             <th>로봇명</th>
+            {renderFunction ? <th>기능</th> : null}
           </tr>
         </thead>
         <tbody>{tableRows}</tbody>
       </Table>
       <Pagination size="sm" className="justify-content-center">
-        {paginationItems}
+        {paginationItems.length ? paginationItems : null}
       </Pagination>
     </div>
   );
-};
-
-export default ParticipantTable;
+}
--- client/src/routes/Participants.jsx
+++ client/src/routes/Participants.jsx
@@ -12,123 +12,157 @@ import EntryForm from '../forms/EntryForm';
 
 export default function Participants() {
   const { competition } = useOutletContext();
-  const [participant, setParticipant] = useState(null);
+  const [participants, setParticipants] = useState();
+  const [targetParticipant, setTargetParticipant] = useState(null);
   const [modification, setModification] = useState(null);
-  const [entryFormData, setEntryFormData] = useState(null);
+  const [entryFormProps, setEntryFormProps] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
   const participantId = searchParams.get('pid');
 
-  const isRegistrationPeriod = checkDateTerm(
-    Date.now(),
-    competition.regDateStart,
-    competition.regDateEnd,
-  );
+  useEffect(() => {
+    async function getParticipants() {
+      try {
+        const res = await axios.get('/api/participants', {
+          params: {
+            competitionId: competition._id,
+            dateSort: 'desc',
+          },
+        });
+        setParticipants(res.data);
+      } catch (err) {
+        alert(err.response?.data);
+      }
+    }
+
+    if (!participants) {
+      getParticipants();
+    }
+  }, [participants, competition]);
 
   useEffect(() => {
+    async function getTargetParticipant() {
+      try {
+        const res = await axios.get(`/api/participants/${participantId}`);
+        setTargetParticipant(res.data);
+      } catch (err) {
+        alert(err.response.data);
+        setTargetParticipant(null);
+      }
+    }
+
     if (participantId) {
-      axios
-        .get(`/api/participants/${participantId}`)
-        .then((res) => setParticipant(res.data))
-        .catch((err) => setParticipant(null));
+      getTargetParticipant();
     } else {
-      setParticipant(null);
+      setTargetParticipant(null);
       setModification(null);
-      setEntryFormData(null);
+      setEntryFormProps(null);
     }
   }, [participantId]);
 
-  return participant ? (
-    modification ? (
-      entryFormData ? (
-        <EntryForm {...entryFormData} />
+  if (targetParticipant) {
+    if (modification) {
+      return entryFormProps ? (
+        <EntryForm {...entryFormProps} />
       ) : (
-        <>
+        <div>
           <p>{modification.message}</p>
           <ParticipantAuthForm
-            participant={participant}
+            participant={targetParticipant}
             method={modification.method}
             onSucceed={modification.onSucceed}
             onCancelled={() => setModification(null)}
           />
-        </>
-      )
-    ) : (
-      <>
-        <div className="text-right">
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => setSearchParams({})}
-          >
-            돌아가기
-          </Button>
         </div>
-        <ParticipantView participant={participant} />
-        <div className={`text-right${isRegistrationPeriod ? ' ' : ' d-none'}`}>
-          <Button
-            variant="primary"
-            size="sm"
-            onClick={() => {
-              setModification({
-                method: 'PATCH',
-                message: '참가 신청 내용을 수정하려면 비밀번호를 입력해주세요.',
-                onSucceed: async (data) => {
-                  const { password } = data;
-                  const resParticipant = await axios.get(
-                    // get more information with authentication
-                    `/api/participants/${participantId}`,
-                    { headers: { Authorization: password } },
-                  );
-                  setEntryFormData({
-                    competition,
-                    data: resParticipant.data,
-                    auth: password,
-                    onSubmitted: (res) => {
-                      setParticipant(res.data);
-                      setModification(null);
-                      setEntryFormData(null);
-                      alert('참가 신청 내용이 수정되었습니다.');
-                    },
-                  });
-                },
-              });
-            }}
-          >
+      );
+    }
+
+    const isRegistrationPeriod = checkDateTerm(
+      Date.now(),
+      competition.regDateStart,
+      competition.regDateEnd,
+    );
+
+    const handlePatchClick = () =>
+      setModification({
+        method: 'PATCH',
+        message: '참가 신청 내용을 수정하려면 비밀번호를 입력해주세요.',
+        onSucceed: async (data) => {
+          try {
+            const { password } = data;
+            const res = await axios.get(
+              // get more information with authentication
+              `/api/participants/${participantId}`,
+              { headers: { Authorization: password } },
+            );
+
+            setEntryFormProps({
+              competition,
+              data: res.data,
+              auth: password,
+              onSubmitted: (res) => {
+                setTargetParticipant(res.data);
+                setModification(null);
+                setEntryFormProps(null);
+                alert('참가 신청 내용이 수정되었습니다.');
+              },
+            });
+          } catch (err) {
+            alert(err.response?.data);
+          }
+        },
+      });
+
+    const handleDeleteClick = () =>
+      setModification({
+        method: 'DELETE',
+        message: '참가 신청을 취소하려면 비밀번호를 입력해주세요.',
+        onSucceed: async (data) => {
+          try {
+            await axios.delete(`/api/participants/${participantId}`, {
+              headers: { Authorization: data.password },
+            });
+            alert('참가 신청이 취소되었습니다.');
+            setSearchParams({});
+            setModification(null);
+          } catch (err) {
+            alert(err.response?.data);
+          }
+        },
+      });
+
+    return (
+      <div className="text-right">
+        <Button
+          variant="secondary"
+          size="sm"
+          onClick={() => setSearchParams({})}
+        >
+          돌아가기
+        </Button>
+        <ParticipantView
+          participant={targetParticipant}
+          className="text-left"
+        />
+        <div className={isRegistrationPeriod ? 'd-block' : 'd-none'}>
+          <Button variant="primary" size="sm" onClick={handlePatchClick}>
             수정
           </Button>{' '}
-          <Button
-            variant="danger"
-            size="sm"
-            onClick={() =>
-              setModification({
-                method: 'DELETE',
-                message: '참가 신청을 취소하려면 비밀번호를 입력해주세요.',
-                onSucceed: async (data) => {
-                  const { password } = data;
-                  await axios.delete(`/api/participants/${participantId}`, {
-                    headers: { Authorization: password },
-                  });
-                  setSearchParams({});
-                  setModification(null);
-                  alert('참가 신청이 취소되었습니다.');
-                },
-              })
-            }
-          >
+          <Button variant="danger" size="sm" onClick={handleDeleteClick}>
             참가 취소
           </Button>
         </div>
-      </>
-    )
-  ) : (
+      </div>
+    );
+  }
+
+  return (
     <ParticipantTable
-      competition={competition}
-      onParticipantClick={(pid) => {
-        setSearchParams({ pid });
+      data={participants}
+      onClick={(p) => {
+        setSearchParams({ pid: p._id });
       }}
-      countPerPage={10}
-      searchParamName={'pid'}
-      className={participant && 'd-none'}
+      renderHref={(p) => `?pid=${p._id}`}
+      countPerPage={15}
     />
   );
 }
