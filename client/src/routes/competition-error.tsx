2a8f70efec3422f5ad46ffe299daae5ab5098f39
DrCloy <alsry0712@gmail.com>
2024-03-21 19:21:17 +0900

    Add competition error page

---
+++ client/src/routes/competition-error.tsx
@@ -0,0 +1,8 @@
+export default function CompetitionError() {
+  return (
+    <div>
+      <h1>Competition not found</h1>
+      <p>Sorry, the competition you are looking for does not exist.</p>
+    </div>
+  );
+}
