7a5435f995c16db0e56a7977e987736d49aa6edb
DrCloy <alsry0712@gmail.com>
2024-03-22 12:19:13 +0900

    Add link in competition error page to redirect

--- client/src/routes/competition-error.tsx
+++ client/src/routes/competition-error.tsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function CompetitionError() {
   return (
     <div>
       <h1>Competition not found</h1>
       <p>Sorry, the competition you are looking for does not exist.</p>
+      <Link className="font-bold text-uos-signature-blue" to="/competitions">
+        Back to competitions
+      </Link>
     </div>
   );
 }
