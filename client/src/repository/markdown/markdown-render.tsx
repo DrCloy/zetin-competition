73de3af2371886574baf3805577c44fa055b49fc
DrCloy <alsry0712@gmail.com>
2024-03-22 12:09:53 +0900

    Add markdown render

---
+++ client/src/repository/markdown/markdown-render.tsx
@@ -0,0 +1,77 @@
+import { MarkdownRenderRepository } from 'core/repository';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+export class MarkdownRender implements MarkdownRenderRepository {
+  render(markdown: string): JSX.Element {
+    return (
+      // react-nmakrdown with gfm plugin
+      // make new tab for link
+      <ReactMarkdown
+        children={markdown}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          a: ({ node, ...props }) => {
+            return (
+              <a
+                {...props}
+                target="_blank"
+                rel="noreferrer noopener"
+                title="새 창에서 열기"
+                className="text-blue-500 underline hover:text-blue-700 "
+              />
+            );
+          },
+          // h4 tag for title
+          h4: ({ node, ...props }) => {
+            return (
+              <h4 {...props} className="font-bold text-2xl">
+                {props.children}
+              </h4>
+            );
+          },
+          // list tag with bullet and margin
+          ul: ({ node, ...props }) => {
+            return <ul {...props} className="list-disc ml-10" />;
+          },
+        }}
+      />
+    );
+  }
+}
+
+// export default function MarkdownRender(props: { markdown: string }) {
+//   return (
+//     // react-nmakrdown with gfm plugin
+//     // make new tab for link
+//     <ReactMarkdown
+//       children={props.markdown}
+//       remarkPlugins={[remarkGfm]}
+//       components={{
+//         a: ({ node, ...props }) => {
+//           return (
+//             <a
+//               {...props}
+//               target="_blank"
+//               rel="noreferrer noopener"
+//               title="새 창에서 열기"
+//               className="text-blue-500 underline hover:text-blue-700 "
+//             />
+//           );
+//         },
+//         // h4 tag for title
+//         h4: ({ node, ...props }) => {
+//           return (
+//             <h4 {...props} className="font-bold text-2xl">
+//               {props.children}
+//             </h4>
+//           );
+//         },
+//         // list tag with bullet and margin
+//         ul: ({ node, ...props }) => {
+//           return <ul {...props} className="list-disc ml-10" />;
+//         },
+//       }}
+//     />
+//   );
+// }
