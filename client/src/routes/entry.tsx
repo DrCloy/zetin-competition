6db112b944c8c02abc0f4f8ede90019195e54601
DrCloy <alsry0712@gmail.com>
2024-03-22 12:47:57 +0900

    Add entry route

---
+++ client/src/routes/entry.tsx
@@ -0,0 +1,24 @@
+import EntryForm from 'components/entry-form';
+import { CompetitionItem } from 'core/model';
+import { useEffect } from 'react';
+import { useNavigate, useOutletContext } from 'react-router-dom';
+import { checkDateTerm } from 'utils';
+
+export default function Entry() {
+  const { competition }: { competition: CompetitionItem } = useOutletContext();
+  const navigate = useNavigate();
+  const isRegisterPeriod = checkDateTerm(
+    new Date(),
+    competition.regDateStart,
+    competition.regDateEnd,
+  );
+
+  useEffect(() => {
+    if (!isRegisterPeriod) {
+      alert('참가 신청 기간이 아닙니다.');
+      navigate(-1);
+    }
+  }, [isRegisterPeriod, navigate]);
+
+  return isRegisterPeriod ? <EntryForm /> : null;
+}
