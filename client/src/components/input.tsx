f3021e87cc1ded4b9b16f3afafe713fb0eead4b6
DrCloy <alsry0712@gmail.com>
2024-03-23 00:20:23 +0900

    Add input componenet

---
+++ client/src/components/input.tsx
@@ -0,0 +1,35 @@
+import { useFormContext } from 'react-hook-form';
+
+export default function Input(props: any) {
+  const { id, label, name, advice, children, ...restProps } = props;
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const error = errors[name]?.message;
+  return (
+    <div className="mb-4">
+      {label && (
+        <label className="inline-block mb-2" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        className={
+          'block w-full px-1 py-2 text-base font-normal leading-normal text-gray-700 bg-white border rounded transition duration-150 ease-in-out focus:outline-0 bg-clip-padding' +
+          (!error
+            ? ' border-gray-300 focus:border-blue-400 focus:box-border focus:shadow-[0px_0px_0px_0.2rem] focus:shadow-[rgba(0,123,255,.25)]'
+            : ' border-red-500 focus:shadow-[0px_0px_0px_0.2rem] focus:shadow-[rgba(220,53,69,.25)] bg-is-invalid bg-no-repeat bg-[right_calc(0.375em+0.1875rem)_center] bg-[length:calc(0.75em+0.375rem)_calc(0.75em+0.375rem)]') +
+          ''
+        }
+        id={id}
+        {...register(name, {
+          required: advice,
+        })}
+        {...restProps}
+      />
+      <div className="w-full mt-1 text-sm text-red-600">{error as string}</div>
+    </div>
+  );
+}
