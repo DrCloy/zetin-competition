d4c6467a8f28d602bbf9161041527c5b9f74282a
DrCloy <alsry0712@gmail.com>
2024-03-30 15:13:54 +0000

    Adjust input padding

--- client/src/components/input.tsx
+++ client/src/components/input.tsx
@@ -29,7 +29,7 @@ export default function Input({
       )}
       <input
         className={
-          'block w-full px-1 py-2 text-base font-normal leading-normal text-gray-700 bg-white border rounded transition duration-150 ease-in-out focus:outline-0 bg-clip-padding' +
+          'block w-full px-3 py-2 text-base font-normal leading-normal text-gray-700 bg-white border rounded transition duration-150 ease-in-out focus:outline-0 bg-clip-padding' +
           (!error
             ? ' border-gray-300 focus:border-blue-400 focus:box-border focus:shadow-[0px_0px_0px_0.2rem] focus:shadow-[rgba(0,123,255,.25)]'
             : ' border-red-500 focus:shadow-[0px_0px_0px_0.2rem] focus:shadow-[rgba(220,53,69,.25)] bg-is-invalid bg-no-repeat bg-[right_calc(0.375em+0.1875rem)_center] bg-[length:calc(0.75em+0.375rem)_calc(0.75em+0.375rem)]')
