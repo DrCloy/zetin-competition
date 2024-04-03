c2f273bb02dc0ee5c9acc21228cded861ad5f5c8
DrCloy <alsry0712@gmail.com>
2024-04-03 06:38:41 +0000

    Add register period and competiton date input

--- client/src/components/forms/competition-form.tsx
+++ client/src/components/forms/competition-form.tsx
@@ -34,24 +34,46 @@ export default function CompetitionForm({
     },
   });
   const { handleSubmit } = form;
-  const checkRegDateEnd = {
-    input: {
-      max: '9999-12-31',
-    },
+
+  const option_regDateStart = {
+    input: {},
     form: {
+      valueAsDate: true,
+    },
+  };
+  const option_regDateEnd = {
+    input: {},
+    form: {
+      valueAsDate: true,
       validate: {
-        minDateTime: (value: Date) =>
-          !competition || value < competition.regDateStart
-            ? '참가 신청 접수 시작일보다 빠를 수 없습니다'
-            : true,
+        minDate: (value: string) => {
+          if (new Date(value) < new Date(form.getValues().regDateStart)) {
+            return '참가 신청 마감일은 시작일보다 빠를 수 없습니다.';
+          }
+          return true;
+        },
+      },
+    },
+  };
+  const option_date = {
+    input: {},
+    form: {
+      valueAsDate: true,
+      validate: {
+        minDate: (value: string) => {
+          if (new Date(value) < new Date(form.getValues().regDateEnd)) {
+            return '참가 신청 접수 종료일보다 빠를 수 없습니다.';
+          }
+          return true;
+        },
       },
     },
   };
 
   const onSubmit = async (data: CompetitionItem) => {
+    console.log(data);
     try {
       let response: CompetitionItem;
-      console.log(data);
       if (!data) {
         response = await repo.competitionDetail.createCompetition(data);
       } else {
@@ -89,6 +111,7 @@ export default function CompetitionForm({
             label="참가 신청 접수 시작"
             id="regDateStart"
             advice="참가 신청 접수 시작일을 입력해주세요."
+            otherOption={option_regDateStart}
           />
           <Input
             type="datetime-local"
@@ -96,12 +119,37 @@ export default function CompetitionForm({
             label="참가 신청 접수 종료"
             id="regDateEnd"
             advice="참가 신청 접수 마감일을 입력해주세요."
-            otherOption={checkRegDateEnd}
+            otherOption={option_regDateEnd}
+          />
+          <Input
+            type="datetime-local"
+            name="date"
+            label="대회 개최일"
+            id="date"
+            advice="대회 실시 날짜를 입력해주세요."
+            otherOption={option_date}
+          />
+        </FieldStack>
+        <FieldStack>
+          <Input
+            type="text"
+            name="place"
+            label="장소"
+            id="place"
+            advice="대회가 열리는 장소를 입력해주세요."
+          />
+          <Input
+            type="text"
+            name="googleMap"
+            label="구글맵 URL"
+            id="googleMap"
+            advice="구글맵 URL을 입력해주세요."
           />
         </FieldStack>
       </Form>
       <button
         type="submit"
+        onClick={handleSubmit(onSubmit)}
         className="cursor-pointer text-white bg-blue-500 border-blue-500 inline-block text-center align-middle border px-3 py-1 rounded transition duration-150 ease-in-out active:bg-blue-600 active:border-blue-600 hover:bg-blue-600 hover:border-blue-600 no-underline focus:outline-0 focus:shadow-[0_0_0_0.2rem] focus:shadow-[rgba(38,143,255,.5)] focus:bg-blue-600 focus:border-blue-600"
       >
         제출
