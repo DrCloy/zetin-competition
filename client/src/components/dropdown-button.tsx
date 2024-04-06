1166ae653a241a1d4077d83de399762436e72401
DrCloy <alsry0712@gmail.com>
2024-04-06 04:15:25 +0000

    Refactor dropdown button for reusing

--- client/src/components/dropdown-button.tsx
+++ client/src/components/dropdown-button.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 
 export default function DropdownButton({
-  items,
+  outerItem,
+  outerOptions,
+  innerItems,
 }: {
-  items: { label: string; onClick: () => void }[];
+  outerItem: string | JSX.Element;
+  outerOptions: object;
+  innerItems: { label: string; onClick: () => void }[];
 }) {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -11,26 +15,13 @@ export default function DropdownButton({
     <div className="relative inline-block text-left -ml-1">
       <div>
         <button
-          className="no-underline inline-flex text-center align-middle justify-center gap-x-1 w-full rounded border border-gray-500 px-2 py-1 bg-gray-500 text-sm font-normal text-white transision ease-in-out duration-150 active:bg-gray-600 active:border-gray-600 active:cursor-pointer focus:outline-0 focus:bg-gray-600 focus:border-gray-600 focus:shadow-[0_0_0_0_0.2rem] focus:shadow-[rgba(130,138,145,.5)]"
           id="options-menu"
           aria-expanded="true"
           aria-haspopup="true"
           onClick={() => setIsOpen(!isOpen)}
+          {...outerOptions}
         >
-          ...
-          <svg
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            className="-mr-1 h-4 w-4"
-          >
-            <path
-              clipRule="evenodd"
-              fillRule="evenodd"
-              d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-            />
-          </svg>
+          {outerItem}
         </button>
       </div>
 
@@ -41,8 +32,8 @@ export default function DropdownButton({
           aria-orientation="vertical"
           aria-labelledby="options-menu"
         >
-          <div className="py-1" role="none">
-            {items.map((item, index) => (
+          <div className="py-2" role="none">
+            {innerItems.map((item, index) => (
               <button
                 key={index}
                 onClick={() => {
--- client/src/routes/file-management.tsx
+++ client/src/routes/file-management.tsx
@@ -106,7 +106,29 @@ export default function FileManagement() {
                 <td className="p-2 break-all">{formatBytes(file.size)}</td>
                 <td className="p-2 break-all">
                   <DropdownButton
-                    items={[
+                    outerItem={
+                      <>
+                        ...
+                        <svg
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                          aria-hidden="true"
+                          className="-mr-1 h-4 w-4"
+                        >
+                          <path
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                          />
+                        </svg>
+                      </>
+                    }
+                    outerOptions={{
+                      className:
+                        'no-underline inline-flex text-center align-middle justify-center gap-x-1 w-full rounded border border-gray-500 px-2 py-1 bg-gray-500 text-sm font-normal text-white transision ease-in-out duration-150 active:bg-gray-600 active:border-gray-600 active:cursor-pointer focus:outline-0 focus:bg-gray-600 focus:border-gray-600 focus:shadow-[0_0_0_0_0.2rem] focus:shadow-[rgba(130,138,145,.5)]',
+                    }}
+                    innerItems={[
                       {
                         label: '파일 ID 복사',
                         onClick: () => showCopyIdDialog(file),
