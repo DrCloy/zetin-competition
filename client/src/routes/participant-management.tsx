c91b5277e13c8701762bbe1843b65c70d47ff64d
DrCloy <alsry0712@gmail.com>
2024-04-06 03:25:59 +0000

    Add participant management router

---
+++ client/src/routes/participant-management.tsx
@@ -0,0 +1,8 @@
+export default function ParticipantManagement() {
+  return (
+    <div>
+      <h1>Participant Management</h1>
+      <p>This is the participant management page.</p>
+    </div>
+  );
+}
