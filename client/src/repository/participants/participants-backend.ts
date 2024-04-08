ba7e0d6956a479c079810bb8edd09193cc93ff66
DrCloy <alsry0712@gmail.com>
2024-04-08 09:22:53 +0000

    Add realOrder in participant model

--- client/src/repository/participants/participants-backend.ts
+++ client/src/repository/participants/participants-backend.ts
@@ -25,6 +25,7 @@ export class ParticipantBackend implements ParticipantManagementRepository {
       robotSensor: data.robotSensor,
       eventName: data.eventName,
       entryOrder: data.entryOrder,
+      realOrder: data.realOrder,
       comment: data.comment,
       privacy: data.privacy,
     })) as ParticipantItem[];
@@ -52,6 +53,7 @@ export class ParticipantBackend implements ParticipantManagementRepository {
       robotSensor: data.robotSensor,
       eventName: data.eventName,
       entryOrder: data.entryOrder,
+      realOrder: data.realOrder,
       comment: data.comment,
       privacy: data.privacy,
     };
@@ -82,6 +84,7 @@ export class ParticipantBackend implements ParticipantManagementRepository {
       robotSensor: data.robotSensor,
       eventName: data.eventName,
       entryOrder: data.entryOrder,
+      realOrder: data.realOrder,
       comment: data.comment,
       privacy: data.privacy,
     };
