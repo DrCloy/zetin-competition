48a4c00bcf47446396dcbad4903e067b2dfecdff
DrCloy <alsry0712@gmail.com>
2024-04-11 00:50:20 +0000

    Add participant manage page

--- client/src/routes/participant-management.tsx
+++ client/src/routes/participant-management.tsx
@@ -1,8 +1,172 @@
+import ParticipantTable from 'components/participant-table';
+import ParticipantView from 'components/participant-view';
+import {
+  CompetitionEvent,
+  CompetitionItem,
+  CompetitionItemMeta,
+  ParticipantItem,
+} from 'core/model';
+import { repo } from 'di';
+import { useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+import { Link } from 'react-router-dom';
+
 export default function ParticipantManagement() {
+  const [competitions, setCompetitions] = useState<CompetitionItemMeta[]>([]);
+  const [targetCompetition, setTargetCompetition] =
+    useState<CompetitionItem | null>(null);
+  const [targetParticipant, setTargetParticipant] =
+    useState<ParticipantItem | null>(null);
+  const [competitionId, setCompetitionId] = useState<string>('');
+
+  const [showForm, setShowForm] = useState(false);
+  const [showView, setShowView] = useState(false);
+
+  useEffect(() => {
+    async function fetchCompetitions() {
+      try {
+        const response = await repo.competitionList.getCompetitions();
+        setCompetitions(response);
+      } catch (error) {
+        alert('대회 목록을 불러오는 중에 오류가 발생했습니다.');
+      }
+    }
+
+    if (competitions.length === 0) {
+      fetchCompetitions();
+    }
+  }, [competitions]);
+
+  const loadCompetition = async (competitionId: string) => {
+    try {
+      const response = await repo.competitionDetail.getCompetitionDetail(
+        competitionId,
+        { moreDetail: true },
+      );
+      setTargetCompetition(response);
+    } catch (error) {
+      alert('대회 정보를 불러오는 중에 오류가 발생했습니다.');
+    }
+  };
+
+  useEffect(() => {
+    if (competitionId) {
+      loadCompetition(competitionId);
+    } else {
+      setCompetitionId('');
+    }
+  }, [competitions, competitionId]);
+
+  const showParticipantEditDialog = async (participant: ParticipantItem) => {
+    setTargetParticipant(participant);
+    console.log(participant);
+  };
+
+  const showUnparticipationDialog = async (participant: ParticipantItem) => {
+    console.log(participant);
+    try {
+      if (
+        window.confirm(
+          `정말로 '${participant.name}'의 참가를 취소하시겠습니까?`,
+        )
+      ) {
+        await repo.participantManager.deleteParticipant(
+          participant.participantId,
+        );
+        alert('참가가 취소되었습니다.');
+        loadCompetition(competitionId);
+      }
+    } catch (error: any) {
+      alert(error?.message || error?.response.data);
+    }
+  };
+
   return (
     <div>
-      <h1>Participant Management</h1>
-      <p>This is the participant management page.</p>
+      <h3 className="text-3xl mb-2">👪 대회 참가자 관리</h3>
+      <p className="mb-4">
+        관리자가 대회 참가자를 수정하거나 삭제할 수 있습니다. 비밀번호를 변경할
+        수도 있어서 대회 참가자가 비밀번호를 잊어버렸을 시에 관리자가 임의의
+        비밀번호로 설정하여 전달할 수 있습니다.
+      </p>
+      <div className="mb-4 flex">
+        <select
+          className="border border-gray-300 rounded-md p-2 w-1/2 px-3 py-1.5 text-base text-gray-700 transition duration-150 ease-in-out
+        focus:border-blue-300 focus:outline-0 focus:shadow-[0_0_0_0.2rem] focus:shadow-[rgba(0,123,255,.25)]"
+          onChange={(e) => {
+            setCompetitionId(e.target.value);
+          }}
+        >
+          <option value="">라인트레이서 대회 선택 ... </option>
+          {competitions &&
+            competitions.map((competition) => (
+              <option key={competition.id} value={competition.id}>
+                {competition.name}
+              </option>
+            ))}
+        </select>
+        {targetCompetition && (
+          <div className="text-right w-1/2 ">
+            <button
+              className="cursor-pointer mr-1 text-white bg-gray-500 border-gray-500 inline-block text-center align-middle px-3 py-1.5 rounded transition duration-150 ease-in0out
+          active:border-gray-600 active:bg-gray-600 hover:bg-gray-600 hover:border-gray-600 focus:outline-0 focus:shadow-[0_0_0_0.2rem] focus:shadow-gray-300"
+            >
+              참가자 등록
+            </button>
+            <Link
+              to={`/api/competitions/${competitionId}/participants?toCSV=true`}
+              target="_blank"
+              rel="noreferrer"
+              className="cursor-pointer mr-1 text-white bg-gray-500 border-gray-500 inline-block text-center align-middle px-3 py-1.5 rounded transition duration-150 ease-in0out
+          active:border-gray-600 active:bg-gray-600 hover:bg-gray-600 hover:border-gray-600 focus:outline-0 focus:shadow-[0_0_0_0.2rem] focus:shadow-gray-300"
+            >
+              CSV 파일로 내보내기
+            </Link>
+          </div>
+        )}
+      </div>
+      {targetCompetition &&
+        targetCompetition.events.map((event: CompetitionEvent) => (
+          <div key={event.id}>
+            <h5 className="text-xl mb-2">'{event.name}' 참가자</h5>
+            <ParticipantTable
+              data={event.participants as ParticipantItem[]}
+              countPerPage={10}
+              onRobotClick={(participant) => {
+                setTargetParticipant(participant);
+                setShowView(true);
+              }}
+              onEditClick={(participant) => {
+                showParticipantEditDialog(participant);
+              }}
+              onUnparticipationClick={(participant) => {
+                showUnparticipationDialog(participant);
+              }}
+            />
+          </div>
+        ))}
+      <ReactModal
+        isOpen={showView}
+        onRequestClose={() => setShowView(false)}
+        shouldCloseOnOverlayClick={false}
+        shouldReturnFocusAfterClose={false}
+        ariaHideApp={false}
+        overlayClassName="fixed top-0 left-0 bg-black bg-opacity-50 z-[1040] w-full h-full transition-opacity ease-linear overflow-y-auto"
+        className="block box-border relative max-w-full md:max-w-3xl w-full mx-auto my-7 transition-transform ease-out duration-300 text-gray-800
+        overflow-y-auto"
+        bodyOpenClassName="overflow-hidden"
+        closeTimeoutMS={200}
+      >
+        <div className="relative flex flex-col w-full bg-white bg-clip-padding border border-white border-opacity-20 rounded-md divide-y divide-slate-300">
+          <div className="flex items-start justify-between p-4 mb-0 text-2xl leading-normal">
+            참가자 정보
+            <button className="float-right" onClick={() => setShowView(false)}>
+              X
+            </button>
+          </div>
+          <ParticipantView participant={targetParticipant!} />
+        </div>
+      </ReactModal>
     </div>
   );
 }
