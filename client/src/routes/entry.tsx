5db553bcfb09d22828bc42913ca1ea6fbcfd27d8
DrCloy <alsry0712@gmail.com>
2024-04-15 07:10:00 +0000

    Error fix

--- client/src/routes/entry.tsx
+++ client/src/routes/entry.tsx
@@ -1,5 +1,6 @@
 import ParticipantForm from 'components/forms/participant-form';
-import { CompetitionItem } from 'core/model';
+import { CompetitionItem, ParticipantInput } from 'core/model';
+import { repo } from 'di';
 import { useEffect } from 'react';
 import { useNavigate, useOutletContext } from 'react-router-dom';
 import { checkDateTerm } from 'utils';
@@ -20,13 +21,18 @@ export default function Entry() {
     }
   }, [isRegisterPeriod, navigate]);
 
+  const onSubmit = async (data: ParticipantInput) => {
+    await repo.participantManager.createParticipant(data);
+    alert('참가 신청이 완료되었습니다.');
+    navigate(-1);
+  };
+
   return isRegisterPeriod ? (
     <ParticipantForm
       competition={competition}
       participant={null}
-      onSubmitted={() => {
-        alert('참가 신청이 완료되었습니다.');
-        navigate(-1);
+      onSubmitted={(data: ParticipantInput) => {
+        onSubmit(data);
       }}
     />
   ) : null;
