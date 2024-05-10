85cf3cd446faa961d5252e8c6d9ec7d8322205fc
DrCloy <alsry0712@gmail.com>
2024-05-10 07:07:58 +0000

    Add environment

--- server/env.ts
+++ server/env.ts
@@ -4,11 +4,26 @@ dotenv.config();
 
 interface IEnv {
   port: string;
+  authURL: string;
+  cookieName: string;
+  cookiePath: string;
   mongoURI: string;
 }
 
 if (process.env.PORT === undefined) {
   process.env.PORT = '8000';
+}
+
+if (process.env.AUTH_URL === undefined) {
+  process.env.AUTH_URL = 'auth.zetin.uos.ac.kr';
+}
+
+if (process.env.COOKIE_NAME === undefined) {
+  process.env.COOKIE_NAME = 'adminToken';
+}
+
+if (process.env.COOKIE_PATH === undefined) {
+  process.env.COOKIE_PATH = '/api';
 }
 
 if (process.env.MONGO_URI === undefined) {
@@ -19,6 +34,9 @@ if (process.env.MONGO_URI === undefined) {
 
 const env: IEnv = {
   port: process.env.PORT,
+  authURL: process.env.AUTH_URL,
+  cookieName: process.env.COOKIE_NAME,
+  cookiePath: process.env.COOKIE_PATH,
   mongoURI: process.env.MONGO_URI,
 };
 
