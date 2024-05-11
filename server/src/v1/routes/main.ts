38bceec4e44d1cccc1188ee73980aa41025b64f5
DrCloy <alsry0712@gmail.com>
2024-05-11 01:02:50 +0000

    Add auth router in main router

--- server/src/v1/routes/main.ts
+++ server/src/v1/routes/main.ts
@@ -1,7 +1,9 @@
+import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 
 import dotenv from 'dotenv';
+import AuthRouter from './api/auth.route';
 
 dotenv.config({
   override: true,
@@ -9,5 +11,22 @@ dotenv.config({
 });
 
 const mainRouter = new Router();
+const authRouter = new AuthRouter({
+  options: {
+    cookieName: process.env.COOKIE_NAME || 'adminToken',
+    cookiePath: process.env.COKIE_PATH || '/api',
+  },
+});
+
+mainRouter.use(async (ctx: Koa.Context, next: Koa.Next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = `Internal Error: ${(err as Error).message}`;
+  }
+});
+
+mainRouter.use('/auth', authRouter.routes());
 
 export default mainRouter;
