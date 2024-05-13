5aa9e68139dd5f313dff44cd92bc6ab6889ac73c
DrCloy <alsry0712@gmail.com>
2024-05-13 08:27:28 +0000

    Add some middleware and auth router

--- server/src/v1/routes/main.ts
+++ server/src/v1/routes/main.ts
@@ -1,32 +1,51 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
+import cookie from 'koa-cookie';
 
 import dotenv from 'dotenv';
 import AuthRouter from './api/auth.route';
+import AuthService from '../service/auth.service';
 
 dotenv.config({
   override: true,
   encoding: 'utf8',
 });
 
-const mainRouter = new Router();
-const authRouter = new AuthRouter({
+const authService = new AuthService({
   options: {
-    cookieName: process.env.COOKIE_NAME || 'adminToken',
-    cookiePath: process.env.COKIE_PATH || '/api',
+    adminID: process.env.ADMIN_ID || 'admin',
+    authURL: process.env.AUTH_URL || 'auth.zetin.uos.ac.kr',
   },
 });
+
+const mainRouter = new Router({
+  prefix: '/api',
+});
+const authRouter = new AuthRouter(
+  {
+    options: {
+      cookieName: process.env.COOKIE_NAME || 'adminToken',
+      cookiePath: process.env.COKIE_PATH || '/api',
+    },
+  },
+  {
+    authService: authService,
+  },
+);
+
+mainRouter.use(cookie());
+mainRouter.use(bodyParser());
 
 mainRouter.use(async (ctx: Koa.Context, next: Koa.Next) => {
   try {
     await next();
   } catch (err) {
     ctx.status = 500;
-    ctx.body = `Internal Error: ${(err as Error).message}`;
+    ctx.body = `${(err as Error).message}`;
   }
 });
 
-mainRouter.use('/auth', authRouter.routes());
+mainRouter.use('/admin', authRouter.routes());
 
 export default mainRouter;
